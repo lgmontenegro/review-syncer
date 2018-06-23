29a7831bb7f7610c4ecea3fa6cf0f87f31635aa3
Leonardo Montenegro <leonardo@montenegro.com.br>
2018-06-23 17:59:32 -0300

    fim do expediente

--- classes.php
+++ classes.php
@@ -11,10 +11,11 @@ final Class Review
 
     private function formatJsonDate($date)
     {
-        $date = explode('T', $review->created_at);
-        $date = implode(' ', $date);
-        $date = explode('.', $date);
-        return $date[0];
+        $date1 = explode('T', $review->created_at);
+        $date2 = implode(' ', $date1);
+        $date3 = explode('.', $date2);
+        
+        return $date3[0];
     }
 
     public function insert($arrayReviews, $app_slug)
@@ -24,10 +25,17 @@ final Class Review
                 continue;
             }
             if(!$this->getByShopifyDomain($review->shop_domain)){
-                $date = $this->formatJsonDate($review->created_at);
+                
+                
+                $date1 = explode('T', $review->created_at);
+                $date2 = implode(' ', $date1);
+                $date3 = explode('.', $date2);
+                
+                return $date3[0];
+
                 $insertStatement = $this->pdo->insert(array('shopify_domain', 'app_slug', 'star_rating', 'created_at'))
                     ->into('shopify_app_reviews')
-                    ->values([$review->shop_domain, $app_slug, $star_rating, $date[0]]);
+                    ->values([$review->shop_domain, $app_slug, $review->star_rating, $date]);
                 $insertReview = $insertStatement->execute(false);
             }else{
                 $this->update($review);
@@ -38,7 +46,17 @@ final Class Review
 
     public function update($review)
     {
-        
+        if($this->getByShopifyDomain($review->shop_domain)){
+            $reviewUpdate = $this->getByShopifyDomain($review->shop_domain);
+            
+            $date = $this->formatJsonDate($review->updated_at);
+            $updateStatement = $this->pdo->update(['previous_start_rating'=>$reviewUpdate->start_rating, 'updated_at'=>$date])
+                ->into('shopify_app_reviews')
+                ->where('shopify_domain', '=', $review->shop_domain);
+            $insertReview = $insertStatement->execute(false);
+        }else{
+            $this->update($review);
+        }
     }
 
     public function getByShopifyDomain($shop_domain)
